04f5d734ac3a8f9253eb977d9a7a55612445219f
brosnan yuen <brosnanyuen@gmail.com>
2024-05-17 18:38:08 -0700

    save

--- tests/test_activation.rs
+++ tests/test_activation.rs
@@ -44,4 +44,20 @@ fn test_neural() {
 
 
 
+
+    let Softplus_out = RayBNN_Neural::Network::Activation::Softplus(&inx);
+
+    let mut Softplus_act_cpu:Vec<f64> = vec![1.163386387166239 ,  0.333452485089616 ,  0.597149103121888  , 1.216537872592923  , 0.976506362205062 ,  0.415989696012672  ,  0.919795750930532   ];
+    let mut Softplus_out_cpu = vec!(f64::default();Softplus_out.elements());
+
+    Softplus_out.host(&mut Softplus_out_cpu);
+
+    Softplus_out_cpu = Softplus_out_cpu.par_iter().map(|x|  (x * 1000000.0).round() / 1000000.0 ).collect::<Vec<f64>>();
+
+    Softplus_act_cpu = Softplus_act_cpu.par_iter().map(|x|  (x * 1000000.0).round() / 1000000.0 ).collect::<Vec<f64>>();
+
+    assert_eq!(Softplus_act_cpu, Softplus_out_cpu);
+
+
+
 }
