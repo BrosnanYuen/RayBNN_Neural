d9b708fc228a54aa714133332d22647488b59a42
brosnan yuen <brosnanyuen@gmail.com>
2024-05-17 18:36:46 -0700

    save

--- tests/test_activation.rs
+++ tests/test_activation.rs
@@ -22,6 +22,24 @@ fn test_neural() {
 
 
 
+    let inx_cpu:Vec<f64> = vec![   0.7888 ,  -0.9269  , -0.2022  ,  0.8652  ,  0.5039  , -0.6619  ,  0.4113 ];
+    let inx = arrayfire::Array::new(&inx_cpu, arrayfire::Dim4::new(&[1, inx_cpu.len() as u64, 1, 1]));
+
+
+
+
+    let ReLU_out = RayBNN_Neural::Network::Activation::ReLU(&inx);
+
+    let mut ReLU_act_cpu:Vec<f64> = vec![ 0.7888 ,  0.0  , 0.0 ,  0.8652  ,  0.5039  , 0.0  ,  0.4113];
+    let mut ReLU_out_cpu = vec!(f64::default();ReLU_out.elements());
+
+
+    ReLU_out.host(&mut ReLU_out_cpu);
+
+    assert_eq!(ReLU_act_cpu, ReLU_out_cpu);
+
+
+
 
 
 
