7938796bfa10d2317c83d28e01b8c81b8097ff7c
brosnan yuen <brosnanyuen@gmail.com>
2024-05-17 17:25:58 -0700

    save

--- src/Network/Activation.rs
+++ src/Network/Activation.rs
@@ -23,46 +23,6 @@ pub fn Softplus(X: &arrayfire::Array<f64>) -> arrayfire::Array<f64>
 }
 
 
-/* 
-pub fn UAF(
-	X: &arrayfire::Array<f64>,
-	A: &arrayfire::Array<f64>,
-	B: &arrayfire::Array<f64>,
-	C: &arrayfire::Array<f64>,
-	D: &arrayfire::Array<f64>,
-	E: &arrayfire::Array<f64>) ->  arrayfire::Array<f64> {
-
-
-	// X + B
-	let mut temp0 = arrayfire::add(X, B, true);
-	// X^2
-	let mut temp1 = arrayfire::pow(X,&TWO_F64,false);
-
-	//A(X + B)  +  C( X^2 )
-	temp0 = arrayfire::mul(A,&temp0,true) + arrayfire::mul(C,&temp1,true);
-
-
-	// X - B;
-	temp1 = arrayfire::sub(X, B, true);
-	// D (X - B)
-	temp1 = arrayfire::mul(D,&temp1,true);
-
-
-	//Softplus( A(X + B)  +  C( X^2 ) )
-	temp0 = Softplus(&temp0);
-	//Softplus( D (X - B) )
-	temp1 = Softplus(&temp1);
-
-	//  Softplus( A(X + B)  +  C( X^2 ) ) - Softplus( D (X - B) )
-	temp0 = temp0 - temp1;
-
-	// Softplus( A(X + B)  +  C( X^2 ) ) - Softplus( D (X - B) ) + E
-	arrayfire::add(&temp0, E, true)
-}
-
-*/
-
-
 
 
 
@@ -113,75 +73,6 @@ pub fn UAF(
 
 
 
-
-
-
-
-/* 
-
-pub fn deriUAF(
-	X: &arrayfire::Array<f64>,
-	A: &arrayfire::Array<f64>,
-	B: &arrayfire::Array<f64>,
-	C: &arrayfire::Array<f64>,
-	D: &arrayfire::Array<f64>,
-	E: &arrayfire::Array<f64>,
-	dX: &mut arrayfire::Array<f64>,
-	dA: &mut arrayfire::Array<f64>,
-	dB: &mut arrayfire::Array<f64>,
-	dC: &mut arrayfire::Array<f64>,
-	dD: &mut arrayfire::Array<f64>,
-	dE: &mut arrayfire::Array<f64>)
-{
-
-	// X + B 
-	let mut temp0 = arrayfire::add(X, B, true);
-	// X^2
-	let mut temp1 = arrayfire::pow(X,&TWO_F64,false);
-	//A(X + B)  +  C( X^2 )
-	let mut expcal0 = arrayfire::mul(A,&temp0,true) + arrayfire::mul(C,&temp1,true);
-
-	//Sigmoid( A(X + B)  +  C( X^2 ) )
-	expcal0 = arrayfire::sigmoid(&expcal0);
-
-	//dA = (X + B) Sigmoid( A(X + B)  +  C( X^2 ) )
-	*dA = arrayfire::mul(&temp0,&expcal0,false);
-	//dB = (A) Sigmoid( A(X + B)  +  C( X^2 ) )
-	*dB = arrayfire::mul(A,&expcal0,true);
-	//dC = (X^2) Sigmoid( A(X + B)  +  C( X^2 ) )
-	*dC = arrayfire::mul(&temp1,&expcal0,false);
-	//  *dC = 0.0f64*arrayfire::mul(&temp1,&expcal0,false);
-
-
-
-	//A + 2Cx
-	temp0 = TWO_F64*arrayfire::mul(C,X,true);
-	temp0 = arrayfire::add(A, &temp0, true);
-	// (A + 2Cx) Sigmoid( A(X + B)  +  C( X^2 ) )
-	expcal0 = arrayfire::mul(&temp0,&expcal0,false);
-
-	// X - B
-	temp0 = arrayfire::sub(X, B, true);
-	//D ( X - B )
-	let mut expcal1 = arrayfire::mul(D,&temp0,true);
-
-	//Sigmoid( D ( X - B )  )
-	expcal1 = arrayfire::sigmoid(&expcal1);
-
-	*dB = dB.clone() + arrayfire::mul(D,&expcal1 ,true);
-
-	//dD = - (X - B) Sigmoid( D ( X - B )  )
-	*dD = -arrayfire::mul(&temp0,&expcal1,false);
-	//dE = 1
-	*dE = arrayfire::constant::<f64>(1.0, X.dims());
-
-	expcal1 = arrayfire::mul(D,&expcal1,true);
-
-	//dX = (A + 2Cx) Sigmoid( A(X + B)  +  C( X^2 ) ) - D Sigmoid( D ( X - B )  )
-	*dX = (expcal0-expcal1);
-}
-
-*/
 
 
 
