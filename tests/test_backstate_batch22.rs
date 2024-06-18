8fc255c8618961c4039bd5197a6828acececa05d
brosnan yuen <brosnanyuen@gmail.com>
2024-06-18 16:57:50 -0700

    save

--- tests/test_backstate_batch22.rs
+++ tests/test_backstate_batch22.rs
@@ -426,8 +426,12 @@ fn test_backstate_batch22() {
 
 
 
-		clusterdiffeq::neural::network_f32::state_space_backward_group2(
-			&netdata,
+		RayBNN_Neural::Network::Neurons::state_space_backward_group2(
+			//&netdata,
+			&modeldata_int,
+
+
+
 			&X,
 		
 		
